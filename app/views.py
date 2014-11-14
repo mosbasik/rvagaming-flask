6b0d9455db1780e54717a8b629d99f3176b60e00
Peter Henry <phenry@tau-enterprise.com>
2014-11-14 10:54:37 -0500

    template inheritance

--- app/views.py
+++ app/views.py
@@ -1,10 +1,31 @@
 from flask import render_template
 from app import app
 
+
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'marseilles'}  # fake user
+    user = {'nickname': 'marseilles'}
+    members = [{
+                    'fbname': 'Joshua Manu',
+                    'stname': 'redsurfable',
+                    'dbid': '0',
+                    'lastseen': '7 months ago',
+                },
+                {
+                    'fbname': 'Christopher Poenaru',
+                    'stname': 'Swambulance',
+                    'dbid': '0',
+                    'lastseen': '7 months ago',
+                },
+                {
+                    'fbname': 'Sam Cho',
+                    'stname': 'Mikasa',
+                    'dbid': '0',
+                    'lastseen': '7 months ago',
+                }]
+
     return render_template('index.html',
                            title='Home',
-                           user=user)
+                           user=user,
+                           members=members)
